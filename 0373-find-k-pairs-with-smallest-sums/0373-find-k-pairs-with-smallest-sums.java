86e6459cc792845f9b41a493b33ac0a6f7a24483
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2023-09-21 01:04:48 +0530

    Time: 31 ms (73.64%), Space: 56.6 MB (77.69%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
@@ -1,31 +1,39 @@
 class Solution {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-        int m = nums1.length;
-        int n = nums2.length;
-
+        // create a min heap which will sort elements on the basis of smallest sum of nums1 + nums2
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[0] - b[0]);
         List<List<Integer>> ans = new ArrayList<>();
-        Set<Pair<Integer, Integer>> visited = new HashSet<>();
 
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b)->(a[0] - b[0]));
-        minHeap.offer(new int[]{nums1[0] + nums2[0], 0, 0});
-        visited.add(new Pair<Integer, Integer>(0, 0));
+        // if either of the given inputs are 0 which means we can not generate any pair
+        if(nums1.length == 0 || nums2.length == 0 || k == 0){
+            return ans;
+        }
 
-        while (k-- > 0 && !minHeap.isEmpty()) {
-            int[] top = minHeap.poll();
-            int i = top[1];
-            int j = top[2];
+        // storing the first n pairs in heap prior to creating the k pairs
+        // creating the pairs by adding all the elements in nums1 with first ele of nums2 one by one
+        for(int i=0; i<nums1.length; i++){
+            pq.add(new int[]{nums1[i] + nums2[0], 0});
+        }
 
-            ans.add(List.of(nums1[i], nums2[j]));
+        // looping until k becomes zero or the heap becomes empty
+        while(k > 0 && !pq.isEmpty()){
+            int[] minPair = pq.poll();
+            // this variable will store the sum of element at nums1 + element at nums2
+            int sum = minPair[0];
+            // this variable will store the curr position of nums2 that is used to create pair with nums1 element
+            int pos = minPair[1];
+            
+            List<Integer> list = new ArrayList<>();
+            list.add(sum - nums2[pos]);
+            list.add(nums2[pos]);
+            ans.add(list);
 
-            if (i + 1 < m && !visited.contains(new Pair<Integer, Integer>(i + 1, j))) {
-                minHeap.offer(new int[]{nums1[i + 1] + nums2[j], i + 1, j});
-                visited.add(new Pair<Integer, Integer>(i + 1, j));
+            // if k pairs are not found and if there are still elements left in nums2 i.e. pos+1 < nums2.length that can make possible min pair with the curr element of nums1 , we add sum of curr elem of nums1 + elem at nums2[pos+1]
+            if(pos + 1 < nums2.length){
+                pq.add(new int[]{sum - nums2[pos] + nums2[pos + 1], pos+1 });
             }
-
-            if (j + 1 < n && !visited.contains(new Pair<Integer, Integer>(i, j + 1))) {
-                minHeap.offer(new int[]{nums1[i] + nums2[j + 1], i, j + 1});
-                visited.add(new Pair<Integer, Integer>(i, j + 1));
-            }
+            // decrement k
+            k--;
         }
 
         return ans;
