5c7111a3f92520673703d06f57a8b4445baf571a
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2023-10-06 15:54:29 +0530

    Time: 0 ms (100%), Space: 39.7 MB (12.08%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int integerBreak(int n) {
+        
+        if(n == 2) return 1;
+        if(n == 3) return 2;
+        if(n == 4) return 4;
+
+        int maxProd = 1;
+        while(n > 4){
+            maxProd *= 3;
+            n -= 3;
+        }
+        maxProd *= n;
+        return maxProd;
+    }
+}
