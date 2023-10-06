d1975874022738bea8cb358a836de098a57bd760
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2023-10-06 16:49:24 +0530

    Time: 0 ms (100%), Space: 39 MB (74.61%) - LeetHub

--- 0343-integer-break/0343-integer-break.java
+++ 0343-integer-break/0343-integer-break.java
@@ -1,16 +1,17 @@
 class Solution {
     public int integerBreak(int n) {
-        
-        if(n == 2) return 1;
-        if(n == 3) return 2;
-        if(n == 4) return 4;
+        if(n <= 3) return n-1;
 
-        int maxProd = 1;
-        while(n > 4){
-            maxProd *= 3;
-            n -= 3;
+        if(n % 3 == 0){
+            return (int)(Math.pow(3, n/3));
         }
-        maxProd *= n;
-        return maxProd;
+
+        else if(n % 3 == 1){
+            return (int)(Math.pow(3, (n-4)/3) * 4);
+        }
+
+        else 
+            return (int)(Math.pow(3, (n-2)/3) * 2);
+
     }
 }
