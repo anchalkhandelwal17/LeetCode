b23e06e38f895584b95afbb2bb69cc1bd9ca0ba9
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2023-10-08 18:26:38 +0530

    Time: 6 ms (14.33%), Space: 42.1 MB (93.61%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,10 +1,8 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         int[] ans = new int[nums1.length];
-
         Arrays.fill(ans, -1);
         for(int i=0; i<nums1.length; i++){
-
             int j=0;
             for(; j<nums2.length; j++){
                 if(nums1[i] == nums2[j]){
@@ -14,7 +12,7 @@ class Solution {
                             break;
                         }
                     }
-                    break;
+                   // break;
                 }
             }
         }
