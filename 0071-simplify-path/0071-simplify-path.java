84fcf8368e2ab281bc993d4f858539f8189c2767
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2023-10-13 00:59:41 +0530

    Time: 4 ms (88.6%), Space: 41.9 MB (97.17%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String simplifyPath(String path) {
+        Stack<String> st = new Stack<>();
+        String[] s = path.split("/");
+
+        for(int i=0; i<s.length; i++){
+            if(!st.isEmpty() && s[i].equals("..")){
+                st.pop();
+            }
+            else if(!s[i].equals("") && !s[i].equals(".") && !s[i].equals("..")){
+                st.push(s[i]);
+            }
+        }
+        if(st.isEmpty()) return "/";
+
+        StringBuilder sb = new StringBuilder();
+        while(!st.isEmpty()){
+            sb.insert(0, st.pop());
+            sb.insert(0, "/");
+        }
+        return sb.toString();    
+    }
+}
