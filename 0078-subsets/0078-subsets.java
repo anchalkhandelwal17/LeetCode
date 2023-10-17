616a64daad14fe1e2539fe0c6a288782fb3f0f59
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2023-10-17 19:02:01 +0530

    Time: 0 ms (100%), Space: 41.7 MB (58.13%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,21 +1,20 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        // TC : O( n * 2 ^ n) - beacuse we are copying the list of size n in base case
-        // SC : O(n * 2 ^ n) // because we are generating the subsets which will be equal to 2 ^ n and extra n for generating and storing a particuar subset
-        List<List<Integer>> ans = new ArrayList<>();
 
-        helper(nums, 0, new ArrayList<>(), ans);
+        List<List<Integer>> ans = new ArrayList<>();
+        solve(0, nums, new ArrayList<>(), ans);
         return ans;
     }
 
-    public void helper(int[] nums, int i, List<Integer> temp , List<List<Integer>> ans){
-        if(i == nums.length){ // if i becomes == nums.length , add the temp as a new list in ans list of lists
-            ans.add(new ArrayList<>(temp));
+    public void solve(int i, int[] nums, List<Integer> list, List<List<Integer>> ans){
+
+        if(i == nums.length){
+            ans.add(new ArrayList<>(list));
             return;
         }
-        temp.add(nums[i]);
-        helper(nums, i+1, temp, ans); // to take 
-        temp.remove(temp.size()-1); // backtrack
-        helper(nums, i+1, temp, ans); // not to take
+        list.add(nums[i]);
+        solve(i+1, nums, list, ans);
+        list.remove(list.size() -1 );
+        solve(i+1, nums, list, ans);
     }
 }
