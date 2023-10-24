0745896686824156865b755752a8afc96822bb9e
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2023-10-25 00:19:32 +0530

    Time: 17 ms (79.11%), Space: 43.8 MB (89.39%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -1,7 +1,20 @@
 class Solution {
     public boolean repeatedSubstringPattern(String s) {
 
-        String str = s + s;
-        return str.substring(1, str.length()-1).contains(s); 
+        int len = s.length();
+
+        for(int i=len/2; i>=1; i--){
+            if(len % i == 0){
+
+                int total_append = len/i;
+                String str = s.substring(0, i);
+                StringBuilder sb = new StringBuilder();
+                for(int append=0; append<total_append; append++){
+                    sb.append(str);
+                }
+                if(sb.toString().equals(s)) return true;
+            }
+        }
+        return false;
     }
 }
