daa33c078b94eceaf1267bfb271372a4bcd0231b
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2023-11-05 02:21:18 +0530

    Time: 1884 ms (38.43%), Space: 45 MB (26.72%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.java
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.java
@@ -1,32 +1,37 @@
 class Solution {
-	public List<Integer> findSubstring(String s, String[] words) {
-		List<Integer> ans = new ArrayList<Integer>();
+    public List<Integer> findSubstring(String s, String[] words) {
+        
+        List<Integer> ans = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
+        int len = words[0].length();
 
-		int len = words[0].length(); // length of each word
+        for(String str : words){
+            map.put(str, map.getOrDefault(str, 0)+1);
+        }
 
-		Map<String, Integer> map = new HashMap<String, Integer>();
-		for (String word : words) map.put(word, map.getOrDefault(word, 0) + 1);
+        for(int i=0; i<=s.length() - len * words.length; i++){
 
+            HashMap<String, Integer> words_map = new HashMap<>(map);
 
-		for (int i = 0; i <= s.length() - len * words.length; i++) {
+            for(int j=0; j<words.length; j++){
 
-			Map<String, Integer> copyMap = new HashMap<String, Integer>(map);
+                String substr = s.substring(i + j*len , i + j*len + len);
 
-			for (int j = 0; j < words.length; j++) {
-				String str = s.substring(i + j*len, i + j*len + len);
+                if(words_map.containsKey(substr)){
+                    if(words_map.get(substr) == 1) words_map.remove(substr);
+                    else words_map.put(substr, words_map.get(substr)-1);
+                    
+                    if(words_map.isEmpty()){
+                        ans.add(i);
+                        break;
+                    }
+                }
+                else{
+                    break;
+                }
+            }
+        }
 
-				// System.out.println(str +" " + len);
-				if (copyMap.containsKey(str)) { // is in remaining words
-					int count = copyMap.get(str);
-					if (count == 1) copyMap.remove(str);
-					else copyMap.put(str, count - 1);
-					if (copyMap.isEmpty()) { // matches
-						ans.add(i);
-						break;
-					}
-				}   else { break; }
-			}
-		}
-		return ans;
-	}
+        return ans;
+    }
 }
