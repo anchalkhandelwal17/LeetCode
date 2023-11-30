c7394684fd5251a3f2432509391b5ec6af13805f
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2023-11-30 18:32:23 +0530

    Time: 2 ms (84.06%), Space: 46.4 MB (41.33%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,23 +1,17 @@
 class Solution {
     public int[] countBits(int n) {
-       
-        int[] ans = new int[n+1];
+        
+        int[] dp = new int[n+1];
+        int offset = 1;
         
         for(int i=1; i<=n; i++){
-            
-            int num = i;
-            int ones = 0;
-            while(num != 0){
-                if((num & 1) == 1){
-                    ones++;
-                }
-                
-                num = num >> 1;
+            if(offset * 2 == i){
+                offset = i;
             }
             
-            ans[i] = ones;
+            dp[i] = 1 + dp[i - offset];
         }
         
-        return ans;
+        return dp;
     }
 }
