18f8069382b6e636e0197e093c900fc622e057d3
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2023-12-05 21:08:27 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (72.99%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.java
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.java
@@ -1,20 +1,22 @@
 class Solution {
     public int numberOfMatches(int n) {
         
-        int matchPlayed = 0;
+//         int matchPlayed = 0;
         
-        while(n != 1){
+//         while(n != 1){
             
-            matchPlayed += n/2;
+//             matchPlayed += n/2;
             
-            if(n % 2 == 0){
-                n = n/2;
-            }
-            else{
-                n = n/2 + 1;
-            }
-        }
+//             if(n % 2 == 0){
+//                 n = n/2;
+//             }
+//             else{
+//                 n = n/2 + 1;
+//             }
+//         }
         
-        return matchPlayed;
+//         return matchPlayed;
+        
+        return n-1;
     }
 }
