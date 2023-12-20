8defd8130e37c4718cd280baa310d542c5551dc4
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2023-12-20 16:41:01 +0530

    Time: 5 ms (89.72%), Space: 46.5 MB (8.61%) - LeetHub

--- 0661-image-smoother/0661-image-smoother.java
+++ 0661-image-smoother/0661-image-smoother.java
@@ -1,24 +1,31 @@
 class Solution {
     public int[][] imageSmoother(int[][] img) {
         
-        int[][] ans = new int[img.length][img[0].length];
+        int m = img.length;
+        int n = img[0].length;
         
-        for(int i=0; i<img.length; i++){
-            for(int j=0; j<img[0].length; j++){
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
                 
-                int count = 0;
                 int sum = 0;
+                int count = 0;
+                
                 for(int r=-1; r<=1; r++){
                     for(int c=-1; c<=1; c++){
-                        if(r+i >= 0 && r+i < img.length && c+j >= 0 && c+j < img[0].length){
-                            sum += img[r+i][c+j];
+                        if(r+i >= 0 && r+i < m && c+j >= 0 && c+j < n){
+                            sum += img[r+i][c+j] & 255;
                             count++;
                         }
                     }
                 }
-                ans[i][j] = sum/count;
+                img[i][j] |= sum / count << 8;
             }
         }
-        return ans;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                img[i][j] = img[i][j] >> 8;
+            }
+        }
+        return img;
     }
 }
