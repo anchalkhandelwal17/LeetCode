38bcb090ee0a2f8a2b07439ae2adfb0902b3e65d
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2023-12-20 16:13:07 +0530

    Time: 1 ms (100.00%), Space: 44.9 MB (5.53%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.java
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.java
@@ -1,10 +1,19 @@
 class Solution {
     public int buyChoco(int[] prices, int money) {
-        Arrays.sort(prices);
-        int chocolate_1 = prices[0];
-        int chocolate_2 = prices[1];
         
-        int moneyLeft = money - (chocolate_1 + chocolate_2);
+        int smallest_price = Integer.MAX_VALUE;
+        int second_smallest_price = Integer.MAX_VALUE;
+        
+        for(int i=0; i<prices.length; i++){
+            if(prices[i] < smallest_price){
+                second_smallest_price = smallest_price;
+                smallest_price = prices[i];
+            }else{
+                second_smallest_price = Math.min(second_smallest_price, prices[i]);
+            }
+        }
+        
+        int moneyLeft = money - (second_smallest_price + smallest_price);
         
         return moneyLeft >= 0 ? moneyLeft : money;
     }
