dde98f4c1bcea8cefb18086deeec2336a58307aa
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2023-12-22 00:18:24 +0530

    Time: 39 ms (21.54%), Space: 73.4 MB (5.00%) - LeetHub

--- 1637-widest-vertical-area-between-two-points-containing-no-points/1637-widest-vertical-area-between-two-points-containing-no-points.java
+++ 1637-widest-vertical-area-between-two-points-containing-no-points/1637-widest-vertical-area-between-two-points-containing-no-points.java
@@ -5,7 +5,7 @@ class Solution {
         int maxArea = Integer.MIN_VALUE;
         
         for(int i=0; i<points.length-2; i++){
-            maxArea = Math.max(maxArea, Math.abs(points[i][0] - points[i+1][0]));
+            maxArea = Math.max(maxArea, points[i+1][0] - points[i][0]);
         }
         return maxArea;
     }
