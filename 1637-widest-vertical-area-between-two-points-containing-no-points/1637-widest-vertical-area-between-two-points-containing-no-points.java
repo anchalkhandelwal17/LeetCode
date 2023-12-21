b9c8347e2d340c9a44c3f291b86836a3284a191a
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2023-12-22 00:17:23 +0530

    Time: 39 ms (21.54%), Space: 73.2 MB (5.00%) - LeetHub

---
+++ 1637-widest-vertical-area-between-two-points-containing-no-points/1637-widest-vertical-area-between-two-points-containing-no-points.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxWidthOfVerticalArea(int[][] points) {
+        Arrays.sort(points, (a,b) -> a[0] - b[0]);
+        
+        int maxArea = Integer.MIN_VALUE;
+        
+        for(int i=0; i<points.length-2; i++){
+            maxArea = Math.max(maxArea, Math.abs(points[i][0] - points[i+1][0]));
+        }
+        return maxArea;
+    }
+}
