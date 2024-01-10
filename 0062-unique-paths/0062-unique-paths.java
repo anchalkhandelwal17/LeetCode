2446ad2ad2d20da00cf4b883216483cb197d1471
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2024-01-10 22:08:35 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (40.69%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,27 +1,17 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        
-        // int N = m + n -2;
-        // int r = m-1;
-        // double  ans = 1;
-
-        // for(int i=1; i<=r; i++){
-        //     ans = ans * (N - r + i)/i;
-        // }
-        // return (int)ans;
+        // TC : O(m*n)  SC : O(m*n) + path len
         int[][] dp = new int[m][n];
-        return helper(0,0,m,n, dp);
+        for(int[] arr : dp) Arrays.fill(arr, -1);
+        int ans = rec(0, 0, m-1, n-1, dp);
+        return ans;
     }
-    public int helper(int i, int j, int m, int n, int[][] dp){
-        if(i == m || j == n){
-            return 0;
-        }
-        if(i == m-1 && j== n-1){
-            return 1;
-        }
-        if(dp[i][j] != 0){
-            return dp[i][j];
-        }
-        return dp[i][j] = helper(i+1, j, m,n, dp) + helper(i, j+1, m,n, dp);
+    
+    public int rec(int i, int j, int m, int n, int[][] dp){
+        if(i == m && j == n) return 1;
+        if(i > m || j > n) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        return dp[i][j] = rec(i+1, j, m, n, dp) + rec(i, j+1, m, n, dp);
     }
 }
