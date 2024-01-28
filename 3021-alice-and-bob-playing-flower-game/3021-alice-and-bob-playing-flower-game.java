2ac9d133aa39bd3c0b6a9f9cb7325862e5241b76
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2024-01-28 20:18:13 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (50.00%) - LeetHub

--- 3021-alice-and-bob-playing-flower-game/3021-alice-and-bob-playing-flower-game.java
+++ 3021-alice-and-bob-playing-flower-game/3021-alice-and-bob-playing-flower-game.java
@@ -1,14 +1,22 @@
 class Solution {
     public long flowerGame(int n, int m) {
+        // long ans = 0;
+        // for(int i=1; i<=n; i++){
+        //     if(i%2 == 1){
+        //         ans += m/2;
+        //     }
+        //     else {
+        //         ans += (m+1)/2;
+        //     }
+        // }
+        // return ans;
+        
         long ans = 0;
-        for(int i=1; i<=n; i++){
-            if(i%2 == 1){
-                ans += m/2;
-            }
-            else {
-                ans += (m+1)/2;
-            }
-        }
-        return ans;
+        long oddX = (n+1)/2;
+        long evenX = n/2;
+        long oddY = (m+1)/2;
+        long evenY = m/2;
+        
+        return (oddX * evenY) + (evenX * oddY);
     }
 }
