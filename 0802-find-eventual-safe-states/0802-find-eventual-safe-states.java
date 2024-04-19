01ea501c56f74422d92e2f56fea93e04a15c2197
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2024-04-20 02:17:07 +0530

    Time: 13 ms (59.3%), Space: 55.4 MB (35.76%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -1,36 +1,51 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
+        // dfs
         
         int n = graph.length;
-        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
-        int[] inDegree = new int[n];
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        int[] vis = new int[n];
+        int[] mark = new int[n];
+        int[] pathVis = new int[n];
         for(int i=0; i<n; i++){
-            list.add(new ArrayList<>());
+            adj.add(new ArrayList<>());
         }
         for(int i=0; i<n; i++){
             for(int it : graph[i]){
-                list.get(it).add(i);
-                inDegree[i]++;
+                adj.get(i).add(it);
             }
         }
-        Queue<Integer> q = new LinkedList<>();
-        ArrayList<Integer> safeNodes = new ArrayList<>();
         for(int i=0; i<n; i++){
-            if(inDegree[i] == 0){
-                q.offer(i);
+            if(vis[i] == 0){
+                dfs(i, adj, vis, pathVis, mark);
             }
         }
-        while(!q.isEmpty()){
-            int node = q.poll();
-            safeNodes.add(node);
-            for(int it : list.get(node)){
-                inDegree[it]--;
-                if(inDegree[it] == 0){
-                    q.offer(it);
+        List<Integer> ans = new ArrayList<>();
+        for(int i=0; i<n; i++){
+            if(mark[i] == 1){
+                ans.add(i);
+            }
+        }
+        return ans;
+    }
+    public boolean dfs(int node, ArrayList<ArrayList<Integer>> adj,
+    int[] vis, int[] pathVis, int[] mark){
+
+        vis[node] = 1;
+        pathVis[node] = 1;
+
+        for(int it : adj.get(node)){
+            if(vis[it] == 0){
+                if(dfs(it, adj, vis, pathVis, mark)){
+                    return true;
                 }
             }
+            else if(pathVis[it] == 1){
+                return true;
+            }
         }
-        Collections.sort(safeNodes);
-        return safeNodes;
+        mark[node] = 1;
+        pathVis[node] = 0;
+        return false;
     }
 }
