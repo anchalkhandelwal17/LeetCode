9f674540685c619a78c1717d0e370882f2d7ec6e
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2024-04-24 21:33:04 +0530

    Time: 0 ms (100%), Space: 39.9 MB (92.34%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,9 +1,17 @@
 class Solution {
     int[] dp = new int[40];
     public int tribonacci(int n) {
+        // recursion
+        // TC : O(3^n)
+        // SC : O(n) , for stack space
+        // if(n == 0) return 0;
+        // if(n == 1) return 1;
+        // if(n == 2) return 1;
+        // return tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3);
+
         // memo
         // TC : O(n) 
-        // SC : O(1)
+        // SC : O(n) , for stack space
         // if(n == 0) return 0;
         // if(n == 1) return 1;
         // if(n == 2) return 1;
