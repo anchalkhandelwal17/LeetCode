8664c3d7fa25cb6932c81708ce3678e7c5ad582d
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2024-05-03 15:34:55 +0530

    Time: 0 ms (100%), Space: 41.4 MB (39.08%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.java
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -1,33 +1,31 @@
 class Solution {
     public int compareVersion(String version1, String version2) {
+        // TC : O(m + n)
+        // SC : O(1)
+        int i = 0, j = 0;
+        int m = version1.length(), n = version2.length();
 
-        String[] v_1 = version1.split("\\.");
-        String[] v_2 = version2.split("\\.");
+        while (i < m || j < n) {
 
-        int i= 0;
-        int j = 0;
+            int val1 = 0;
+            int val2 = 0;
 
-        while(i < v_1.length && j < v_2.length){
-            if(Integer.parseInt(v_1[i]) > Integer.parseInt(v_2[j])){
+            while(i < m && version1.charAt(i) != '.'){
+                val1 = val1 * 10 + version1.charAt(i)-'0';
+                i++;
+            }
+            while(j < n && version2.charAt(j) != '.'){
+                val2 = val2 * 10 + version2.charAt(j)-'0';
+                j++;
+            }
+
+            if(val1 > val2){
                 return 1;
             }
-            if(Integer.parseInt(v_1[i]) < Integer.parseInt(v_2[j])){
+            else if(val1 < val2){
                 return -1;
             }
             i++;
-            j++;
-        }
-        while(i < v_1.length){
-            if(Integer.parseInt(v_1[i]) > 0){
-                return 1;
-            }
-            i++;
-        }
-
-        while(j < v_2.length){
-            if(Integer.parseInt(v_2[j]) > 0){
-                return -1;
-            }
             j++;
         }
         return 0;
