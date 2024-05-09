5ecaa07e4751cb80ba43f454f61eaff19ee0ae25
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2024-05-09 18:06:04 +0530

    Time: 88 ms (40.58%), Space: 58.4 MB (68.85%) - LeetHub

--- 0502-ipo/0502-ipo.java
+++ 0502-ipo/0502-ipo.java
@@ -1,21 +1,23 @@
 class Solution {
     public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
-        PriorityQueue<int[]> pqMin = new PriorityQueue<>((a,b) -> a[0]-b[0]);
-        PriorityQueue<int[]> pqMax = new PriorityQueue<>((a,b) -> b[1]-a[1]);
+        // TC : O(nlogn)
+        // SC : O(n)
+        PriorityQueue<int[]> minPq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        PriorityQueue<int[]> maxPq = new PriorityQueue<>((a, b) -> b[1] - a[1]);
 
         for(int i=0; i<profits.length; i++){
-            pqMin.add(new int[]{capital[i], profits[i]});
+            minPq.offer(new int[]{capital[i], profits[i]});
         }
 
-        while(k-- > 0){
-            while(!pqMin.isEmpty() && pqMin.peek()[0] <= w){
-                pqMax.add(pqMin.poll());
+        while(k > 0){
+            while(!minPq.isEmpty() && minPq.peek()[0] <= w){
+                maxPq.offer(minPq.poll());
             }
-            if(pqMax.isEmpty()) break;
-
-            w += pqMax.poll()[1];
+            if(maxPq.isEmpty()) break;
+            w += maxPq.peek()[1];
+            maxPq.poll();
+            k--;
         }
-
         return w;
     }
 }
