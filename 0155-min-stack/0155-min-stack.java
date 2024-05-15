14ad651ab64028b2b0ff4df3d9dd84483455786c
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2024-05-15 17:35:28 +0530

    Time: 4 ms (95.53%), Space: 44.4 MB (87.35%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,30 +1,31 @@
 class MinStack {
-    Stack<Integer> st = new Stack<>();
-    int min = Integer.MAX_VALUE;
+    // TC : O(1)
+    // SC : O(n + n)
+    Stack<int[]> st;
+    int min;
     public MinStack() {
-        
+        st = new Stack<>();
+        min = Integer.MAX_VALUE;
     }
     
     public void push(int val) {
-        if(val <= min){
-            st.push(min);
-            min = val;
-        }
-        st.push(val);
+        min = Math.min(min, val);
+        st.push(new int[]{val, min});
     }
     
     public void pop() {
-        if(st.pop() == min){
-            min = st.pop();
+        int[] val = st.pop();
+        if(val[1] == min){
+            min = st.isEmpty() ? Integer.MAX_VALUE : st.peek()[1];
         }
     }
     
     public int top() {
-        return st.peek();
+        return st.peek()[0];
     }
     
     public int getMin() {
-        return min;
+        return st.peek()[1];
     }
 }
 
