0e4a16d9d07a3154ca0d802e23876b4ed050567e
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2024-05-22 00:50:07 +0530

    Time: 12 ms (80.73%), Space: 48 MB (67.76%) - LeetHub

--- 0436-find-right-interval/0436-find-right-interval.java
+++ 0436-find-right-interval/0436-find-right-interval.java
@@ -1,28 +1,50 @@
 class Solution {
     public int[] findRightInterval(int[][] intervals) {
-        // TC : O(n*n)
-        // SC : O(n)
+
         int n = intervals.length;
         int[] ans = new int[n];
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int[] start = new int[n];
+        for(int i=0; i<n; i++){
+            map.put(intervals[i][0], i);
+            start[i] = intervals[i][0];
+        }
+
+        Arrays.sort(start);
 
         for(int i=0; i<n; i++){
-            int start = intervals[i][0];
             int end = intervals[i][1];
-            int idx = -1;
-            int min = Integer.MAX_VALUE;
-            for(int j=0; j<n; j++){
-                if(intervals[j][0] == end){
-                    idx = j;
-                    break;
-                }
-                if(intervals[j][0] >= end){
-                    if(intervals[j][0] < min){
-                        min = intervals[j][0];
-                        idx = j;
-                    }
-                }
+
+            int val = find(start, end);
+            if(val != Integer.MAX_VALUE){
+                ans[i] = map.get(val);
             }
-            ans[i] = idx;
+            else{
+                ans[i] = -1;
+            }
+        }
+        return ans;
+    }
+
+    public int find(int[] arr, int val){
+
+        int lo = 0;
+        int hi = arr.length-1;
+        int ans = Integer.MAX_VALUE;
+
+        while(lo <= hi){
+            int m = (lo + hi)/2;
+
+            if(arr[m] == val){
+                return val;
+            }
+            else if(arr[m] > val){
+                ans = arr[m];
+                hi = m-1;
+            }
+            else{
+                lo = m+1;
+            }
         }
         return ans;
     }
