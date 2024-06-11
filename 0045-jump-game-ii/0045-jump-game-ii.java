4d90b912f18cbc1373feff772f5af168e8843f84
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2024-06-11 19:23:12 +0530

    Time: 1 ms (97.82%), Space: 44.6 MB (89.05%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,19 +1,25 @@
 class Solution {
     public int jump(int[] nums) {
-        if(nums.length <= 1)return 0;
+        // TC : O(n)
+        // SC : O(1);
+
+        int n = nums.length;
+        if(n <= 1) return 0;
         int jumps = 0;
-        int nextJump = 0;
-        int maxReachable = 0;
-        
-        for(int i=0; i<nums.length; i++){
-            if(i > maxReachable) return -1;
+        int maxReach = 0;
+        int timeToJump = 0;
+        for(int i=0; i<n; i++){
+            if(i > maxReach) return -1;
             
-            maxReachable = Math.max(maxReachable, nums[i]+i);
-            if(i == nextJump){
+            maxReach = Math.max(maxReach, i + nums[i]);
+
+            if(i == timeToJump){
+                timeToJump = maxReach;
                 jumps++;
-                nextJump = maxReachable;
                 
-                if(nextJump >= nums.length-1) return jumps;
+                if(timeToJump >= n-1){
+                    return jumps;
+                }
             }
         }
         return jumps;
