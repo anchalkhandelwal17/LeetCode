35480afcbb1118da985532a19e2c55188d58fd5f
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2024-07-14 22:18:28 +0530

    Time: 1 ms (99.48%), Space: 42 MB (92.76%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.java
+++ 0257-binary-tree-paths/0257-binary-tree-paths.java
@@ -1,41 +1,46 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public List<String> binaryTreePaths(TreeNode root) {
+        // TC : O(n)
+        // SC : total no. of root to leaf paths + dfs stack space of (h), 
+        // max space used by StringBuilder which'll be (h)) 
         ArrayList<String> list = new ArrayList<>();
-        if(root == null) return list;
+        if (root == null)
+            return list;
         StringBuilder sb = new StringBuilder();
         allPaths(root, list, sb);
         return list;
     }
 
-    public void allPaths(TreeNode root, List<String> list, StringBuilder sb){
-        if(root == null) return;
+    public void allPaths(TreeNode root, List<String> list, StringBuilder sb) {
+        if (root == null)
+            return;
 
         int len = sb.length();
         sb.append(root.val);
 
-        if(root.left == null && root.right == null){
+        if (root.left == null && root.right == null) {
             list.add(sb.toString());
+        } else {
+            sb.append("->");
+            allPaths(root.left, list, sb);
+            allPaths(root.right, list, sb);
+
         }
-
-        sb.append("->");
-        allPaths(root.left, list, sb);
-        allPaths(root.right, list, sb);
-
         sb.setLength(len);
     }
 }
