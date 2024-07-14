a6824efece3f123688ce61ad8540efddbb3f820f
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2024-07-14 23:48:22 +0530

    Time: 1 ms (98.58%), Space: 43.1 MB (67.88%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -24,10 +24,11 @@
  }
 class Solution {
     public int widthOfBinaryTree(TreeNode root) {
+        // That's the binary tree property. You would just need to know it beforehand. left is n * 2, right is n * 2 + 1
         if(root == null) return 0;
 
         Queue<Pair> q = new LinkedList<>();
-        q.offer(new Pair(root, 0));
+        q.offer(new Pair(root, 1));
         long ans = 0;
 
         while(!q.isEmpty()){
@@ -40,8 +41,8 @@ class Solution {
                 if(i == 0) first = pair.idx;
                 if(i == size - 1) second = pair.idx;
 
-                if(node.left != null) q.offer(new Pair(node.left, 2*pair.idx+1));
-                if(node.right != null) q.offer(new Pair(node.right, 2*pair.idx+2)); 
+                if(node.left != null) q.offer(new Pair(node.left, 2*pair.idx));
+                if(node.right != null) q.offer(new Pair(node.right, 2*pair.idx+1)); 
             }
             ans = Math.max(ans, second - first + 1);
         }
