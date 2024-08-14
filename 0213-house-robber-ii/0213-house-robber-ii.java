03036e8475742c4912bb75ef9a24ee923a651753
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2024-08-14 22:44:34 +0530

    Time: 0 ms (100%), Space: 40.4 MB (97.48%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,34 +1,35 @@
 class Solution {
     public int rob(int[] nums) {
-        if(nums.length == 1) return nums[0];
-        if(nums.length == 2) return Math.max(nums[0], nums[1]);
-        
-        int[] dp = new int[nums.length];
+        // TC : O(n)
+        // SC : O(n)
+        int n = nums.length;
+        if(n == 0) return 0;
+        else if(n == 1) return nums[0];
+        if(n == 2) return Math.max(nums[0], nums[1]);
+        int[] dp = new int[n];
         Arrays.fill(dp, -1);
-        
-        int ans1 = solve(nums, 0,  dp);
+        int ans1 = solve(0, n-1, nums, dp);
         Arrays.fill(dp, -1);
-        int ans2 = solve2(nums, 1, dp);
+        int ans2 = solve(1, n, nums, dp);
         return Math.max(ans1, ans2);
     }
-    
-    public int solve(int[] nums, int i, int[] dp){
-        if(i >= nums.length-1) return 0;
-        if(dp[i] != -1) return dp[i];
-        
-        int take = nums[i] + solve(nums, i+2, dp);
-        int notTake = 0 + solve(nums, i+1, dp);
-        
-        return dp[i] = Math.max(take, notTake);
-    }
-    
-    public int solve2(int[] nums, int i, int[] dp){
-        if(i >= nums.length) return 0;
-        if(dp[i] != -1) return dp[i];
-        
-        int take = nums[i] + solve2(nums, i+2, dp);
-        int notTake = 0 + solve2(nums, i+1, dp);
-        
-        return dp[i] = Math.max(take, notTake);
+
+    public int solve(int i, int n, int[] nums, int[] dp){
+        // base condition
+        if(i >= n){
+            return 0;
+        }
+
+        // if dp array already contains ans of a state return it
+        if(dp[i] != -1){
+            return dp[i];
+        }
+        // pick this idx and move to idx+2
+        int idxPlusTwo = nums[i] + solve(i+2, n, nums, dp);
+
+        // not pick this idx and move to i+1
+        int idxPlusOne = 0 + solve(i+1, n, nums, dp);
+
+        return dp[i] = Math.max(idxPlusTwo, idxPlusOne);
     }
 }
