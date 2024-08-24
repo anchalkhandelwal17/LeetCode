8a4a0fc09d03cdabc31532ad246f2f93a30afea2
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2024-08-24 17:11:13 +0530

    Time: 8 ms (50.76%), Space: 57.5 MB (40.56%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -1,61 +1,20 @@
-// class Solution {
-//     int max = Integer.MIN_VALUE;
-//     public int maximalSquare(char[][] matrix) {
-//         // TC : O(m * n)   SC : O(m * n) + recursion space
-//         if(matrix.length == 0) return 0;
-//         int row = matrix.length;
-//         int col = matrix[0].length;
-//         int[][] dp = new int[row][col];
-//         for(int[] arr : dp){
-//             Arrays.fill(arr, -1);
-//         }
-        
-//         solve(matrix, 0, 0, dp);
-//         return max * max;
-//     }
-    
-//     public int solve(char[][] mat, int i, int j, int[][] dp){
-//         if(i >= mat.length || j >= mat[0].length){
-//             return 0;
-//         }
-//         if(dp[i][j] != -1) return dp[i][j];
-        
-//         int right = solve(mat, i, j+1, dp);
-//         int diagonal = solve(mat, i+1, j+1, dp);
-//         int down = solve(mat, i+1, j, dp);
-        
-//         if(mat[i][j] == '1'){
-//             dp[i][j] = 1 + Math.min(Math.min(right, diagonal), down);
-//             max = Math.max(max, dp[i][j]);
-//             return dp[i][j];
-//         }
-//         else return dp[i][j] = 0;
-//     }
-// }
-
 class Solution {
-    int max = Integer.MIN_VALUE;
     public int maximalSquare(char[][] matrix) {
-        // TC : O(m * n)   SC : O(m * n)
-        if(matrix.length == 0) return 0;
-        int row = matrix.length;
-        int col = matrix[0].length;
-        
-        int[][] dp = new int[row+1][col+1];
-        int maxSquare = Integer.MIN_VALUE;
-        
-        for(int i=row-1; i>=0; i--){
-            for(int j=col-1; j>=0; j--){
-                if(matrix[i][j] == '1'){
-                    int right = dp[i][j+1];
-                    int diagonal = dp[i+1][j+1];
-                    int down = dp[i+1][j];
-                    
-                    dp[i][j] = 1 + Math.min(Math.min(right, diagonal), down);
-                    maxSquare = Math.max(maxSquare, dp[i][j]);
+        // TC : O(m*n)
+        // SC : O(m*n)
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] dp = new int[m+1][n+1];
+        int ans = 0;
+        for(int i=1; i<=m; i++){
+            for(int j=1; j<=n; j++){
+                if(matrix[i-1][j-1] == '1'){
+                    dp[i][j] = 1 + Math.min(dp[i-1][j-1],
+                    Math.min(dp[i][j-1], dp[i-1][j]));
+                    ans = Math.max(ans, dp[i][j]);
                 }
             }
         }
-        return maxSquare * maxSquare;
+        return ans*ans;
     }
 }
