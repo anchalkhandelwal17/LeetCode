57ba11f6c5fd9c569520f81a30685d580314be79
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2024-09-12 00:19:44 +0530

    Time: 4 ms (75.1%), Space: 45.4 MB (5.94%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,29 +1,32 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        // TC : quadratic
-        // SC : O(n * m) + recursion stack space
-        int m = word1.length();
-        int n = word2.length();
-        int[][] dp = new int[m][n];
-        for(int[] arr : dp) Arrays.fill(arr, -1);
-        return solve(m-1, n-1, word1, word2, dp);
+        // TC : O(m * n)
+        // SC : O(m * n) + rec space
+        int n = word1.length();
+        int m = word2.length();
+        int[][] dp = new int[n+2][m+2];
+        for(int[] arr : dp){
+            Arrays.fill(arr, -1);
+        }
+        return solve(word1.length()-1, word2.length()-1, word1, word2, dp);
     }
+
     public int solve(int i, int j, String a, String b, int[][] dp){
+        // base conditions
         if(i < 0) return j+1;
         if(j < 0) return i+1;
+        if(dp[i][j] != -1) return dp[i][j];
 
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
         if(a.charAt(i) == b.charAt(j)){
             return solve(i-1, j-1, a, b, dp);
         }
+        int insert = 1 + solve(i, j-1, a, b, dp);
         int delete = 1 + solve(i-1, j, a, b, dp);
         int replace = 1 + solve(i-1, j-1, a, b, dp);
-        int insert = 1 + solve(i, j-1, a, b, dp);
-        return dp[i][j] = Math.min(delete, Math.min(replace, insert));
+
+        return dp[i][j] = Math.min(insert, Math.min(delete, replace));
     }
 }
-// f(i, j)
-// base case - if i becomes less than 0  OR if j becomes less than 0 return 
-//  f(i, j) => min(f(i-1, j), (i-1, j-1), (i, j-1))
+
+// f(i, j) = Math.min(f(i-1, j-1), Math.min(i, j-1, i-1, j))
+// if(a.ch(i) == b.ch(i)) f(i-1, j-1)
