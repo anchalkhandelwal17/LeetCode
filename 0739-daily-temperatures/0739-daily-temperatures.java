c3151590f7ce69bb8fc72ca3a91e1412c56349cb
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2024-09-17 23:23:11 +0530

    Time: 79 ms (25.81%), Space: 60 MB (72.72%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,14 +1,24 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
+        // TC : O(n + n)
+        // SC : O(n + n)
+        int n = temperatures.length;
+        int[] ans = new int[n];
         Stack<int[]> st = new Stack<>();
-        int[] ans = new int[temperatures.length];
-        for(int i=temperatures.length-1; i>=0; i--){
-            while(!st.isEmpty() && st.peek()[0] <= temperatures[i]){
+
+        for(int i=n-1; i>=0; i--){
+            int val = temperatures[i];
+            while(!st.isEmpty() && st.peek()[1] <= val){
                 st.pop();
             }
-            ans[i] = st.isEmpty() ? 0 : st.peek()[1] - i;
-            st.push(new int[]{temperatures[i], i});
+            if(st.isEmpty()) ans[i] = 0;
+            else ans[i] = st.peek()[0] - i;
+
+            st.push(new int[]{i, temperatures[i]});
         }
         return ans;
     }
 }
+
+// we can apply brute force to solve but that will be quadratic time coomplexity
+// to solve it in a better time complexity , stack can we be used
