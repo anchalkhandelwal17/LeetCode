d00bf9da5f1b7891c4a4052a9e8bbc7f35ecc92a
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2024-09-20 17:46:18 +0530

    Time: 0 ms (100%), Space: 41.6 MB (66.65%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,55 +1,31 @@
-// class Solution {
-//     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-//         // TC : O(m*n)   SC : O(m*n) + length of the path
-//         int m = obstacleGrid.length;
-//         int n = obstacleGrid[0].length;
-//         if(obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1) return 0;
-        
-//         int[][] dp = new int[m][n];
-//         for(int[] arr : dp){
-//             Arrays.fill(arr, -1);
-//         } 
-//         int ans = rec(obstacleGrid, 0, 0, m-1, n-1, dp);
-//         return ans;
-//     }
-    
-//     public int rec(int[][] grid, int i, int j, int m, int n, int[][] dp){
-//         if(i == m && j == n) return 1;
-//         if(i > m || j > n) return 0;
-//         if(grid[i][j] == 1) return 0;
-//         if(dp[i][j] != -1) return dp[i][j];
-        
-//         return dp[i][j] = rec(grid, i+1, j, m, n, dp) + rec(grid, i, j+1, m, n, dp);
-//     }
-// }
-
-
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        // TC : O(m*n)   SC : O(m*n)
+        // TC : O(m * n)
+        // SC : O(m * n) + recur space
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
-        if(obstacleGrid[0][0] == 1|| obstacleGrid[m-1][n-1] == 1) return 0;
-        int[][] dp = new int[m][n];
-        
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                int down = 0;
-                int right = 0;
-                if(i == 0 && j == 0) dp[i][j] = 1;
-                else if(obstacleGrid[i][j] == 1) dp[i][j] = 0;
-                else{
-                    if(i > 0){
-                        down += dp[i-1][j];
-                    }
-                    if(j > 0){
-                        right += dp[i][j-1];
-                    }
-                    dp[i][j] = down + right;
-                }
-            }
+        int[][] dp = new int[m+1][n+1];
+        for(int[] arr : dp){
+            Arrays.fill(arr, -1);
         }
-        return dp[m-1][n-1];
+
+        return solve(0, 0, m, n, obstacleGrid, dp);
     }
 
+    public int solve(int i, int j, int m, int n, int[][] grid, int[][] dp){
+        if(i == m-1 && j == n-1 && grid[i][j] == 0){
+            return  1;
+        }
+        if(i >= m || j >= n || grid[i][j] == 1){
+            return 0;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+
+        int down = solve(i+1, j, m, n, grid, dp);
+        int right = solve(i, j+1, m, n, grid, dp);
+
+        return dp[i][j] = down + right;
+    }
 }
