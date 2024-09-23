afcc745fb57556d001d522b3503fa6306e8bba8a
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2024-09-23 23:52:50 +0530

    Time: 0 ms (100%), Space: 42.1 MB (73.14%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,28 +1,28 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        if(nums.length == 1 || nums[0] > nums[1]){
-            return 0;
-        }
-        else if(nums[nums.length-1] > nums[nums.length-2]){
-            return nums.length-1;
-        }
-        else{
-            int start = 1;
-            int end = nums.length-2;
+        // TC : O(log n)
+        // SC : O(1)
+        int n = nums.length;
+        if(n == 1) return 0;
+        else if(nums[0] > nums[1]) return 0;
+        else if(nums[n-1] > nums[n-2]) return n-1;
 
-            while(start <= end){
-                int mid = (start + end)/2;
-                if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid + 1]){
-                    return mid;
-                }
-                else if(nums[mid] > nums[mid-1]){
-                    start = mid+1;
-                }
-                else{
-                    end = mid-1;
-                }
+        int lo = 0;
+        int hi = nums.length-1;
+        while(lo <= hi){
+            int m = (lo + hi)/2;
+
+            if(m-1 >= 0 && m+1 < n && nums[m] > nums[m-1] && nums[m] > nums[m+1]){
+                return m;
             }
+            else if(m-1 >= 0 && nums[m-1] > nums[m]){
+                hi = m;
+            }
+            else lo = m;
         }
         return -1;
     }
 }
+
+// brute force can be applied
+// think of some other optimised approach 
