2726603cbfcd453b86bb14748b8287d363cbaaea
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2024-10-05 23:40:52 +0530

    Time: 4 ms (99.19%), Space: 42.4 MB (88.85%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,36 +1,45 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
         // TC : O(m + n*26)
-        // SC : O(26) + O(26)
+        // SC : O(26 + 26)
+
         int m = s1.length();
         int n = s2.length();
+
+        // freq arr for s1
         int[] s1Freq = new int[26];
+
+        // freq arr for s2
         int[] s2Freq = new int[26];
 
         for(int i=0; i<m; i++){
             char c = s1.charAt(i);
             s1Freq[c - 'a']++;
         }
-        int i=0;
-        int j=0;
+
+        int i=0, j=0;
+
         while(j < n){
             char c = s2.charAt(j);
             s2Freq[c - 'a']++;
+
             if(j - i + 1 < m){
                 j++;
             }
             else{
-                if(check(s1Freq, s2Freq)){
+                if(isFreqSame(s1Freq, s2Freq)){
                     return true;
                 }
                 s2Freq[s2.charAt(i) - 'a']--;
+
                 i++;
                 j++;
             }
         }
         return false;
     }
-    public boolean check(int[] arr1, int[] arr2){
+
+    public boolean isFreqSame(int[] arr1, int[] arr2){
         for(int i=0; i<26; i++){
             if(arr1[i] != arr2[i]) return false;
         }
