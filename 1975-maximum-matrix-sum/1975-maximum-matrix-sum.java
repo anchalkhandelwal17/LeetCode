4fc2a5ff22108ec7f291937d51a8f9f061932fb7
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2024-11-24 23:57:35 +0530

    Time: 6 ms (91.07%), Space: 54.4 MB (99.54%) - LeetHub

--- 1975-maximum-matrix-sum/1975-maximum-matrix-sum.java
+++ 1975-maximum-matrix-sum/1975-maximum-matrix-sum.java
@@ -1,22 +1,29 @@
 class Solution {
     public long maxMatrixSum(int[][] matrix) {
-        // TC : O(n*n)
+        // TC : O(m * n)
         // SC : O(1)
-        int n = matrix.length;
-        long sum = 0;
-        int minVal = Integer.MAX_VALUE;
-        int neg = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if(matrix[i][j] < 0) neg++;
-                sum += Math.abs(matrix[i][j]);
+        int m = matrix.length;
+        int n = matrix[0].length;
 
-                if(minVal > Math.abs(matrix[i][j])){
-                    minVal = Math.abs(matrix[i][j]);
+        long ans = 0;
+        int cnt = 0;
+        int smallestNegative = Integer.MAX_VALUE;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(matrix[i][j] <= 0){
+                    cnt++;
                 }
+
+                smallestNegative = Math.min(Math.abs(matrix[i][j]), smallestNegative);
             }
         }
-        if(neg % 2 == 0) return sum;
-        return sum - 2*minVal;
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                ans += Math.abs(matrix[i][j]);
+            }
+        }
+        if(cnt % 2 == 0) return ans;
+        return ans - (2 * smallestNegative);
     }
 }
