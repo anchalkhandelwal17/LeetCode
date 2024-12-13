c5a96f71cffbf3906f5232ee6029ba94e44bd7fe
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2024-12-13 18:18:03 +0530

    Time: 180 ms (45.59%), Space: 57.5 MB (74.54%) - LeetHub

--- 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.java
+++ 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.java
@@ -1,36 +1,35 @@
 class Solution {
     public long findScore(int[] nums) {
-        int n = nums.length;
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b ) -> {
-            if(a[0] == b[0]){
-                return a[1] - b[1];
-            }
+        // TC : O(nlogn)
+        // SC : O(n)
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
+            if(a[0] == b[0]) return a[1] - b[1];
             return a[0] - b[0];
         });
-        
+
+        int n = nums.length;
+
         for(int i=0; i<n; i++){
             pq.offer(new int[]{nums[i], i});
         }
 
         long score = 0;
-        while(!pq.isEmpty()){
-            int val = pq.peek()[0];
+        while(pq.size() > 0){
+            int num = pq.peek()[0];
             int idx = pq.peek()[1];
             pq.poll();
-            if(nums[idx] == -1){
-                continue;
-            }
+            if(nums[idx] == -1) continue;
             else{
-                score += val;
+                score += num;
                 nums[idx] = -1;
-                if(idx-1 >= 0){
-                    nums[idx-1] = -1;
-                }
-                if(idx+1 < n){
-                    nums[idx+1] = -1;
-                }
+
+                if(idx-1 >= 0) nums[idx-1] = -1;
+                if(idx+1 < n) nums[idx+1] = -1; 
             }
         }
         return score;
+
     }
 }
+
+// two ds coming to mind is map and pq
