f6f9ec2816c36eded31e85f656f5769595aa4ed6
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2025-01-09 23:36:10 +0530

    Time: 9 ms (73.65%), Space: 45.3 MB (12.13%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,37 +1,32 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        // TC : O(n^2)
-        // SC : O(n*2005)
-        int[][] dp = new int[nums.length + 2][2005];
+        // TC : O(n * 2000)
+        // SC : O(n * 2000)
+
+        int n = nums.length;
+        int[][] dp = new int[n+1][2001];
         for(int[] arr : dp){
-            Arrays.fill(arr, (int)(1e6));
+            Arrays.fill(arr, -1);
         }
-        return solve(0, 0, nums, target, dp);
+        return solve(0, nums, target, 0, dp);
     }
 
-    public int solve(int i, int sum, int[] nums, int target, int[][] dp){
-        // base condition
+    public int solve(int i, int[] nums, int target, int sum, int[][] dp){
+        // if(sum > target) return 0;
+
         if(i >= nums.length){
-            if(sum == target) return 1;
-            else return 0;
+            return sum == target ? 1 : 0;
         }
-        if(sum < 0){
-            if(dp[i][1000 + Math.abs(sum)] != (int)(1e6)) return dp[i][1000 + Math.abs(sum)];
-        }
-        if(sum >= 0) if(dp[i][sum] != (int)(1e6)) return dp[i][sum];
 
-        int take = 0 + solve(i+1, sum+nums[i], nums, target, dp);
-        int not_take = 0 + solve(i+1, sum-nums[i], nums, target, dp);
+        if(dp[i][sum + 1000] != -1) return dp[i][sum + 1000];
 
-        if(sum < 0){
-            dp[i][1000 + Math.abs(sum)] = take + not_take;
-        }
-        else dp[i][sum] = take + not_take;
-        return take + not_take; 
+        int plusSign = solve(i+1, nums, target, sum + nums[i], dp);
+        int negSign = solve(i+1, nums, target, sum - nums[i], dp);
+
+        return dp[i][sum + 1000] = plusSign + negSign;
     }
 }
 
-// the possibility of sum that is equal to target should include all numbers in arr
-// f(nums, target, i, sum) = sum(0 + f(num, target, i+1, sum+nums[i],
-//                           0 + f(nums, target, i+1, sum-nums[i])));
-// base case => if(i >= n) => if(sum == t) return 1 else return 0  
+// we can use pick and not_pick dp
+// we can assign +sign before every value
+// also we can assign -sign before every value
