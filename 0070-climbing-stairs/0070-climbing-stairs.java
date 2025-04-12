e89e95a36baa645cb2a59fda8b4ca7eda6bde93f
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2025-04-12 14:55:18 +0530

    Time: 0 ms (100%), Space: 40.5 MB (38.4%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,19 +1,28 @@
 class Solution {
+    int[] dp = new int[50];
+    {
+        Arrays.fill(dp, -1);
+    }
+
     public int climbStairs(int n) {
         // TC : O(n)
         // SC : O(n)
-        int[] dp = new int[n+1];
-        Arrays.fill(dp, -1);
-        int distinctWays = solve(0, n, dp);
-        return distinctWays;
-    }
-    public int solve(int i, int n, int[] dp){
-        if(i > n) return 0;
-        if(i == n) return 1;
-        if(dp[i] != -1) return dp[i];
+        if (n == 0)
+            return 1;
+        if (n < 0)
+            return 0;
+        if (dp[n] != -1)
+            return dp[n];
 
-        int oneStep = solve(i+1, n, dp);
-        int twoSteps = solve(i+2, n, dp);
-        return dp[i] = oneStep + twoSteps;
+        int oneStep = climbStairs(n - 1);
+        int twoStep = climbStairs(n - 2);
+
+        return dp[n] = oneStep + twoStep;
     }
 }
+
+// at a point i can take 1 step or 2
+// so here i have two choices at a particular point of time
+// take either a step 1 or take 2 
+// and if reached the top return 1 to the ans
+// now let's memoize this solution
