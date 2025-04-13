d643ff6c3665dc888f57cc0d2d8d7d7b7e447e2b
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2025-04-13 21:39:26 +0530

    Time: 11 ms (13.15%), Space: 46.7 MB (55.1%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,38 +1,35 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-
-        // First sorting the array
-        Arrays.sort(intervals, new Comparator<int[]>(){
-            public int compare(int[] a, int[]b){
-                return a[0] - b[0];
+        // TC : O(nlogn)
+        // SC : O(n)
+        int n = intervals.length;
+        List<int[]> list = new ArrayList<>();
+        Arrays.sort(intervals, (a, b) -> {
+            if(a[0] == b[0]){
+                return a[1] - b[1];
             }
+            return a[0] - b[0];
         });
 
-        // creating a list of lists and storing all the non-overlapping intervals lists in this list
-        List<List<Integer>> list = new ArrayList<>();
+        for(int i=0; i<n; i++){
+            int a = intervals[i][0];
+            int b = intervals[i][1];
 
-        // iterating through all the intervals in the array
-        for(int i=0; i<intervals.length; i++){
-
-            // creating a new list if the list is empty or if the start of current interval is greater than the end of previous interval 
-            if(list.isEmpty()||intervals[i][0]>list.get(list.size()-1).get(1)){
-                list.add(Arrays.asList(intervals[i][0], intervals[i][1]));
-                
+            if(list.isEmpty() || list.get(list.size()-1)[1] < a){
+                list.add(new int[]{a, b});
             }
-
-            // updating the current list's end if the start of current interval is less than equal to the previous interval's end
-                else{
-                    list.get(list.size()-1).set(1, Math.max(list.get(list.size()-1).get(1), intervals[i][1]));
-                }
-            
+            else{
+                list.get(list.size() - 1)[1] = Math.max(list.get(list.size() - 1)[1], b);
+            }
         }
 
-        // creating a new ans array of size list which was used to store all the non-overlapping intervals
         int[][] ans = new int[list.size()][2];
-        for(int i=0; i<list.size(); i++){
-            ans[i][0] = list.get(i).get(0);
-            ans[i][1] = list.get(i).get(1);
+
+        for(int i=0; i<ans.length; i++){
+            ans[i][0] = list.get(i)[0];
+            ans[i][1] = list.get(i)[1];
         }
+
         return ans;
     }
 }
