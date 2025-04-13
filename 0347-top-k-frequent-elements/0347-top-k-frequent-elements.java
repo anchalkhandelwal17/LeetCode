61176c12c80b66fb3e0cb53cd3a9fd3649b1671a
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2025-04-13 22:28:39 +0530

    Time: 14 ms (60.08%), Space: 49.4 MB (7.99%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,33 +1,33 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
+        // TC : O(nlogk)
+        // Sc : O(n)
         int n = nums.length;
         HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        for(int i=0; i<n; i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
         }
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> {
+            if(a[0] == b[0]){
+                return a[1] - b[1];
+            }
+            return a[0] - b[0];
+        });
 
-        for (int key : map.keySet()) {
+        for(int key : map.keySet()){
             int K = key;
             int V = map.get(K);
 
-            pq.offer(new int[] { K, V });
-            if (pq.size() > k)
-                pq.poll();
+            minHeap.offer(new int[]{V, K});
+            if(minHeap.size() > k) minHeap.poll();
         }
 
-        int[] ans = new int[pq.size()];
+        int[] ans = new int[Math.min(k, minHeap.size())];
         int idx = 0;
-
-        while (!pq.isEmpty()) {
-            int[] arr = pq.poll();
-            int K = arr[0];
-           
-            ans[idx++] = K;
-
+        while(!minHeap.isEmpty()){
+            ans[idx++] = minHeap.poll()[1];
         }
-
         return ans;
     }
 }
