46599fc09f6b0f720f66d02a3951d1b986d7e51a
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2025-04-22 23:58:15 +0530

    Time: 0 ms (100%), Space: 40.8 MB (91.03%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -3,33 +3,28 @@ class Solution {
         // TC : O(n)
         // SC : O(n)
         int n = nums.length;
-        if(n == 0) return 0;
-        else if(n == 1) return nums[0];
         if(n == 2) return Math.max(nums[0], nums[1]);
-        int[] dp = new int[n];
+        if(n == 1) return nums[0];
+        int[] dp = new int[n+1];
         Arrays.fill(dp, -1);
-        int ans1 = solve(0, n-1, nums, dp);
+        int ans1 = solve(nums, 0, n-1, dp);
         Arrays.fill(dp, -1);
-        int ans2 = solve(1, n, nums, dp);
+        int ans2 = solve(nums, 1, n, dp);
         return Math.max(ans1, ans2);
     }
 
-    public int solve(int i, int n, int[] nums, int[] dp){
-        // base condition
-        if(i >= n){
+    public int solve(int[] nums, int i, int n, int[] dp){
+        // base case
+        if(i >= n) {
             return 0;
         }
-
-        // if dp array already contains ans of a state return it
         if(dp[i] != -1){
             return dp[i];
         }
-        // pick this idx and move to idx+2
-        int idxPlusTwo = nums[i] + solve(i+2, n, nums, dp);
 
-        // not pick this idx and move to i+1
-        int idxPlusOne = 0 + solve(i+1, n, nums, dp);
+        int take = nums[i] + solve(nums, i+2, n, dp);
+        int not_take = 0 + solve(nums, i+1, n, dp);
 
-        return dp[i] = Math.max(idxPlusTwo, idxPlusOne);
+        return dp[i] = Math.max(take, not_take);
     }
 }
