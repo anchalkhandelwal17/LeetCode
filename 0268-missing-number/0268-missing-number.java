80fb8c89953bfc80c7a30717c30351dbe8555a6a
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2025-04-24 01:10:19 +0530

    Time: 1 ms (35.02%), Space: 45.3 MB (71.29%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,36 +1,24 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        
+        int i = 0;
         int n = nums.length;
-        
-        int i=0;
-        
-        while(i<nums.length){
-            
-            if(nums[i] == n){
-                i++;
-            }
+        while(i < n){
+            if(nums[i] == n) i++;
             else if(nums[i] != i){
                 swap(nums, i, nums[i]);
             }
-            else{
-                i++;
-            }
+            else i++;
         }
-        
         for(int k=0; k<n; k++){
             if(nums[k] != k){
                 return k;
             }
         }
-        
         return n;
     }
-    
-    
-    public void swap(int[] nums, int first, int second){
-        int temp = nums[first];
-        nums[first] = nums[second];
-        nums[second] = temp;
+    public void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
