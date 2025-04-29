fc7b24e2c064e58a64555872a6e985d48e4b1e2f
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2025-04-29 21:19:25 +0530

    Time: 4 ms (99.02%), Space: 42.5 MB (88.89%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,47 +1,38 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
         // TC : O(m + n*26)
-        // SC : O(26 + 26)
-
+        // SC : O(26)
         int m = s1.length();
         int n = s2.length();
+        if (m > n) return false;
 
-        // freq arr for s1
-        int[] s1Freq = new int[26];
+        int[] arr1 = new int[26];
+        int[] arr2 = new int[26];
 
-        // freq arr for s2
-        int[] s2Freq = new int[26];
-
-        for(int i=0; i<m; i++){
-            char c = s1.charAt(i);
-            s1Freq[c - 'a']++;
+        for (int i = 0; i < m; i++) {
+            arr1[s1.charAt(i) - 'a']++;
         }
 
-        int i=0, j=0;
+        int i = 0;
+        for (int j = 0; j < n; j++) {
+            arr2[s2.charAt(j) - 'a']++;
 
-        while(j < n){
-            char c = s2.charAt(j);
-            s2Freq[c - 'a']++;
-
-            if(j - i + 1 < m){
-                j++;
-            }
-            else{
-                if(isFreqSame(s1Freq, s2Freq)){
-                    return true;
-                }
-                s2Freq[s2.charAt(i) - 'a']--;
-
+            if (j - i + 1 > m) {
+                arr2[s2.charAt(i) - 'a']--;
                 i++;
-                j++;
+            }
+
+            if (j - i + 1 == m && check(arr1, arr2)) {
+                return true;
             }
         }
+
         return false;
     }
 
-    public boolean isFreqSame(int[] arr1, int[] arr2){
-        for(int i=0; i<26; i++){
-            if(arr1[i] != arr2[i]) return false;
+    public boolean check(int[] arr1, int[] arr2) {
+        for (int i = 0; i < 26; i++) {
+            if (arr1[i] != arr2[i]) return false;
         }
         return true;
     }
