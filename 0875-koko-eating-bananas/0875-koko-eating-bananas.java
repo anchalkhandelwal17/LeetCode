53bdba78d7f1c4f019586d6187113374d9cc51b3
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2025-04-30 00:09:44 +0530

    Time: 11 ms (77.6%), Space: 45.8 MB (5.95%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,43 +1,28 @@
-
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        // TC : O(nlog(1e8))
-        // SC : O(1)
-        int lo = 1;
-        int hi = (int)(1e9);
-
+        int min = 1;
+        int max = (int)(1e9);
         int ans = -1;
 
-        while (lo <= hi) {
-            int m = (lo + hi) / 2;
-            if (check(piles, h, m)) {
-                ans = m;
-                hi = m - 1;
-            } else
-                lo = m + 1;
+        while(min <= max){
+            int mid = (min + max) / 2;
+
+            if(check(piles, h, mid)){
+                ans = mid;
+                max = mid-1;
+            }
+            else min = mid+1;
         }
         return ans;
     }
 
-    public boolean check(int[] piles, int h, int x) {
-        long ans = 0;
-        int n = piles.length;
-        for (int i = 0; i < n; i++) {
-            ans = ans + (piles[i] / x);
-            if (piles[i] % x != 0)
-                ans++;
+    public boolean check(int[] nums, int h, int k){
+        long cnt = 0;
+        for(int i=0; i<nums.length; i++){
+            int x = nums[i] / k;
+            cnt += x;
+            if(nums[i] % k != 0) cnt++;
         }
-        return ans <= h;
+        return cnt <= h;
     }
 }
-
-// Input: piles = [3,6,7,11], h = 8
-// Output: 4
-
-// 1 + 2 + 2 + 3
-
-// pick a number x
-// if a value p is div by x then add p/x to ans
-// other wise add p/x + 1 to ans
-
-// at the end if ans <= h true else false;
