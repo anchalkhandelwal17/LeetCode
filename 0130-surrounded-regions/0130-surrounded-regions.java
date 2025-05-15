f6cb4f1a2f0f41e6871c677a71221347ddf2ef6e
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2025-05-16 00:50:05 +0530

    Time: 2 ms (84.25%), Space: 46.1 MB (12.99%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,17 +1,17 @@
 class Solution {
     public void solve(char[][] board) {
-        // TC : O(m*n*4)
+        // TC : O(m*n)
         // SC : O(m*n)
         int m = board.length;
         int n = board[0].length;
         for(int i=0; i<n; i++){
-            if(board[0][i] == 'O') bfs(0, i, m, n, board);
-            if(board[m-1][i] == 'O') bfs(m-1, i, m, n, board);
+            if(board[0][i] == 'O') dfs(0, i, m, n, board);
+            if(board[m-1][i] == 'O') dfs(m-1, i, m, n, board);
         }
 
         for(int i=0; i<m; i++){
-            if(board[i][0] == 'O') bfs(i, 0, m, n, board);
-            if(board[i][n-1] == 'O') bfs(i, n-1, m, n, board);
+            if(board[i][0] == 'O') dfs(i, 0, m, n, board);
+            if(board[i][n-1] == 'O') dfs(i, n-1, m, n, board);
         }
 
         for(int i=0; i<m; i++){
@@ -26,29 +26,44 @@ class Solution {
         }
     }
 
-    public void bfs(int i, int j, int m, int n, char[][] board){
-        Queue<int[]> q = new LinkedList<>();
-        int[] rowDir = {-1, 0, 1, 0};
-        int[] colDir = {0, 1, 0, -1};
-        q.offer(new int[]{i, j});
-        board[i][j] = '#';
-        while(!q.isEmpty()){
-            int row = q.peek()[0];
-            int col = q.peek()[1];
+    // BFS
+    // public void bfs(int i, int j, int m, int n, char[][] board){
+    //     Queue<int[]> q = new LinkedList<>();
+    //     int[] rowDir = {-1, 0, 1, 0};
+    //     int[] colDir = {0, 1, 0, -1};
+    //     q.offer(new int[]{i, j});
+    //     board[i][j] = '#';
+    //     while(!q.isEmpty()){
+    //         int row = q.peek()[0];
+    //         int col = q.peek()[1];
 
-            q.poll();
+    //         q.poll();
 
-            for(int k=0; k<4; k++){
-                int newRow = row + rowDir[k];
-                int newCol = col + colDir[k];
+    //         for(int k=0; k<4; k++){
+    //             int newRow = row + rowDir[k];
+    //             int newCol = col + colDir[k];
 
-                if(newRow < m && newRow >= 0 &&
-                newCol < n && newCol >= 0 &&
-                board[newRow][newCol] == 'O'){
-                    q.offer(new int[]{newRow, newCol});
-                    board[newRow][newCol] = '#';
-                }
+    //             if(newRow < m && newRow >= 0 &&
+    //             newCol < n && newCol >= 0 &&
+    //             board[newRow][newCol] == 'O'){
+    //                 q.offer(new int[]{newRow, newCol});
+    //                 board[newRow][newCol] = '#';
+    //             }
+    //         }
+    //     }
+    // }
+
+    // DFS
+
+    public void dfs(int i, int j, int m, int n, char[][] board){
+        if(i >= 0 && i < m && j >= 0 && j < n &&
+            board[i][j] == 'O'){
+
+                board[i][j] = '#';
+                dfs(i-1, j, m, n, board);
+                dfs(i, j+1, m, n, board);
+                dfs(i+1, j, m, n, board);
+                dfs(i, j-1, m, n, board);
             }
-        }
     }
 }
