e5ab0d62850bb437b12f2416f8103b0a329e0813
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2025-05-16 23:54:15 +0530

    Time: 0 ms (100%), Space: 42.7 MB (81.38%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,20 +1,21 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-
         List<List<Integer>> ans = new ArrayList<>();
-        solve(0, nums, new ArrayList<>(), ans);
+        solve(0, nums.length, nums, ans, new ArrayList<>());
         return ans;
     }
 
-    public void solve(int i, int[] nums, List<Integer> list, List<List<Integer>> ans){
-
-        if(i == nums.length){
+    public void solve(int i, int n, int[] nums, List<List<Integer>> ans, List<Integer> list){
+        if(i >= n){
             ans.add(new ArrayList<>(list));
             return;
         }
+
         list.add(nums[i]);
-        solve(i+1, nums, list, ans);
-        list.remove(list.size() -1 );
-        solve(i+1, nums, list, ans);
+        solve(i+1, n, nums, ans, list);
+        list.remove(list.size()-1);
+        solve(i+1, n, nums, ans, list);
     }
 }
+
+// simple take not_take recursion
