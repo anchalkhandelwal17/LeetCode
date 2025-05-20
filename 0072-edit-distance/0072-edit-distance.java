573a308192711c77f777c81354c2eaa0c030aee2
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2025-05-21 02:01:48 +0530

    Time: 5 ms (63%), Space: 45.2 MB (31.1%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,32 +1,35 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        // TC : O(m * n)
-        // SC : O(m * n) + rec space
-        int n = word1.length();
-        int m = word2.length();
-        int[][] dp = new int[n+2][m+2];
+        // TC : O(m*n)
+        // SC : O(m*n + m+n)
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m][n];
         for(int[] arr : dp){
             Arrays.fill(arr, -1);
         }
-        return solve(word1.length()-1, word2.length()-1, word1, word2, dp);
+        return solve(word1, word2, 0, 0, dp);
     }
 
-    public int solve(int i, int j, String a, String b, int[][] dp){
-        // base conditions
-        if(i < 0) return j+1;
-        if(j < 0) return i+1;
+    public int solve(String a, String b, int i, int j, int[][] dp){
+        // base
+        if(i == a.length() && j == b.length()) return 0;
+        if(i >= a.length()) return b.length() - j;
+        if(j >= b.length()) return a.length() - i;
         if(dp[i][j] != -1) return dp[i][j];
-
+        
         if(a.charAt(i) == b.charAt(j)){
-            return solve(i-1, j-1, a, b, dp);
+            return solve(a, b, i+1, j+1, dp);
         }
-        int insert = 1 + solve(i, j-1, a, b, dp);
-        int delete = 1 + solve(i-1, j, a, b, dp);
-        int replace = 1 + solve(i-1, j-1, a, b, dp);
+        int insert = 1 + solve(a, b, i, j+1, dp);
+        int delete = 1 + solve(a, b, i+1, j, dp);
+        int replace = 1 + solve(a, b, i+1, j+1, dp);
 
         return dp[i][j] = Math.min(insert, Math.min(delete, replace));
     }
 }
 
-// f(i, j) = Math.min(f(i-1, j-1), Math.min(i, j-1, i-1, j))
-// if(a.ch(i) == b.ch(i)) f(i-1, j-1)
+// we need to convert word1 to word2
+// insert a char will be 1 + (i, j+1)
+// delete a char will be 1 + (i+1, j)
+// replace a char will be 1 + (i+1, j+1)
