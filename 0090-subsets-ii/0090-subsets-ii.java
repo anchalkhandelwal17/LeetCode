1864b1bc8a15dced248fbb4f9e28a5373e6fc561
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2025-05-21 00:43:55 +0530

    Time: 1 ms (99.92%), Space: 43.3 MB (95.89%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,25 +1,23 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        // TC : O(2^n * n)
-        // SC : O(2^n * n) + recursion space
         Arrays.sort(nums);
         List<List<Integer>> ans = new ArrayList<>();
-        solve(0, nums, new ArrayList<>(), ans);
+        solve(0, nums, ans, new ArrayList<>());
         return ans;
     }
 
-    public void solve(int i, int[] nums, List<Integer> list, List<List<Integer>> ans){
+    public void solve(int i, int[] nums, List<List<Integer>> ans, List<Integer> list){
         if(i >= nums.length){
             ans.add(new ArrayList<>(list));
             return;
         }
 
         list.add(nums[i]);
-        solve(i+1, nums, list, ans);
+        solve(i+1, nums, ans, list);
         list.remove(list.size()-1);
         while(i + 1 < nums.length && nums[i] == nums[i+1]){
             i++;
         }
-        solve(i+1, nums, list, ans);
+        solve(i+1, nums, ans, list);
     }
 }
