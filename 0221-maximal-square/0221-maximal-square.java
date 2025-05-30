80e73077a640d332f4102a0aa62b3a79b9d87fb7
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2025-05-30 21:46:11 +0530

    Time: 11 ms (18.49%), Space: 60.2 MB (65.68%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -1,20 +1,44 @@
 class Solution {
     public int maximalSquare(char[][] matrix) {
         // TC : O(m*n)
-        // SC : O(m*n)
+        // SC : O(m*n) + rec space
         int m = matrix.length;
         int n = matrix[0].length;
-        int[][] dp = new int[m+1][n+1];
-        int ans = 0;
-        for(int i=1; i<=m; i++){
-            for(int j=1; j<=n; j++){
-                if(matrix[i-1][j-1] == '1'){
-                    dp[i][j] = 1 + Math.min(dp[i-1][j-1],
-                    Math.min(dp[i][j-1], dp[i-1][j]));
-                    ans = Math.max(ans, dp[i][j]);
-                }
+        int[] ans = new int[1];
+
+        int[][] dp = new int[m][n];
+        for (int[] arr : dp) {
+            Arrays.fill(arr, -1);
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                solve(i, j, matrix, dp, ans);
             }
         }
-        return ans*ans;
+
+        return ans[0] * ans[0];
+    }
+
+    public int solve(int i, int j, char[][] matrix, int[][] dp, int[] ans) {
+        if (i >= matrix.length || j >= matrix[0].length) {
+            return 0;
+        }
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        if (matrix[i][j] == '1') {
+            int right = solve(i, j + 1, matrix, dp, ans);
+            int down = solve(i + 1, j, matrix, dp, ans);
+            int diagonal = solve(i + 1, j + 1, matrix, dp, ans);
+
+            dp[i][j] = 1 + Math.min(right, Math.min(down, diagonal));
+            ans[0] = Math.max(ans[0], dp[i][j]);
+        } else {
+            dp[i][j] = 0;
+        }
+
+        return dp[i][j];
     }
 }
