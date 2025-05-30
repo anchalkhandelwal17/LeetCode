2fb4e88be72a7736b3c90213fadac4f100246f63
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2025-05-31 02:51:02 +0530

    Time: 0 ms (100%), Space: 42.2 MB (21.13%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,23 +1,22 @@
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-        // TC : O(quadratic)
-        // SC : O(m * n) + rec space
+        // TC : O(m * n)
+        // SC : O(m * n) + O(m + n)
         int m = s1.length();
         int n = s2.length();
-        int p = s3.length();
-        if(m + n != p) return false;
+        int k = s3.length();
+
+        if(m + n != k) return false;
         Boolean[][] dp = new Boolean[m][n];
-        return solve(m-1, n-1, p-1, s1, s2, s3, dp);   
+        return solve(m-1, n-1, k-1, s1, s2, s3, dp);
     }
 
-    public boolean solve(int i, int j, int k, String a, String b, String c,
-    Boolean[][] dp){
+    public boolean solve(int i, int j, int k, String a, String b, String c, Boolean[][] dp){
         if(i >= 0 && j >= 0 && dp[i][j] != null) return dp[i][j];
         if(k < 0) return true;
 
-        if((i >= 0 && a.charAt(i) == c.charAt(k) && solve(i-1, j, k-1, a, b, c, dp))
-        || (j >= 0 && b.charAt(j) == c.charAt(k) && solve(i, j-1, k-1, a, b, c, dp))){
-           if(i >= 0 && j >= 0) dp[i][j] = true;
+        if((i >= 0 && a.charAt(i) == c.charAt(k) && solve(i-1, j, k-1, a, b, c, dp)) || (j >= 0 && b.charAt(j) == c.charAt(k) && solve(i, j-1, k-1, a, b, c, dp))){
+            if(i >= 0 && j >= 0) dp[i][j] = true;
             return true;
         }
 
