85ccd7e8ae884ec0f91fab4b225c7987e4604045
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2025-06-24 20:21:34 +0530

    Time: 6 ms (98.1%), Space: 48 MB (56.45%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,25 +1,22 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        // TC : O(n * k)
-        // SC : O(n * k)
+        // TC : O(n * max(strs[i]log(strs[i])))
         int n = strs.length;
-        HashMap<String, List<String>> map = new HashMap<>();
         List<List<String>> ans = new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
 
         for(int i=0; i<n; i++){
             String str = strs[i];
             char[] ch = str.toCharArray();
             Arrays.sort(ch);
-            String sortedStr = new String(ch);
-
-            if(!map.containsKey(sortedStr)){
-                map.put(sortedStr, new ArrayList<>());
+            String newStr = new String(ch);
+            if(!map.containsKey(newStr)){
+                map.put(newStr, new ArrayList<>());
             }
-            map.get(sortedStr).add(str);
+            map.get(newStr).add(str);
         }
-
-        for(String key : map.keySet()){
-            ans.add(new ArrayList<>(map.get(key)));
+        for(String k : map.keySet()){
+            ans.add(map.get(k));
         }
         return ans;
     }
