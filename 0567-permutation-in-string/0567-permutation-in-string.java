8ea3fce9a044ea0a61604611f877957996e84112
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2025-08-21 00:08:52 +0530

    Time: 4 ms (99.01%), Space: 42.8 MB (67.94%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,36 +1,33 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        // TC : O(n * 26)
-        // SC : O(26)
         int m = s1.length();
         int n = s2.length();
-        if (m > n)
-            return false;
-
-        int[] freq1 = new int[26];
-        int[] freq2 = new int[26];
-        for (int k = 0; k < m; k++) {
-            char c = s1.charAt(k);
-            freq1[c - 'a']++;
+        int[] arr1 = new int[26];
+        int[] arr2 = new int[26];
+        for(int i=0; i<m; i++){
+            arr1[s1.charAt(i) - 'a']++;
         }
-        int i = 0;
-        for (int j = 0; j < n; j++) {
-            char c = s2.charAt(j);
-            freq2[c - 'a']++;
 
-            if (j - i + 1 == m && check(freq1, freq2)) {
-                return true;
-            }
-            if (j - i + 1 == m) {
-                freq2[s2.charAt(i) - 'a']--;
+        int i=0;
+        int j=0;
+        while(j < n){
+            arr2[s2.charAt(j) - 'a']++;
+
+            if(j - i + 1 == m){
+                if(check(arr1, arr2)) return true;
+                arr2[s2.charAt(i) - 'a']--;
                 i++;
             }
+            j++;
         }
         return false;
     }
+
     public boolean check(int[] arr1, int[] arr2){
         for(int i=0; i<26; i++){
-            if(arr1[i] != arr2[i]) return false;
+            if(arr1[i] != arr2[i]){
+                return false;
+            }
         }
         return true;
     }
