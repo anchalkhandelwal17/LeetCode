08d6f26cfe5436118a62f3b45ef237745a266acc
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2026-05-10 17:20:20 +0530

    Time: 108 ms (77.35%), Space: 122.7 MB (25.53%) - LeetHub

--- 3927-minimize-array-sum-using-divisible-replacements/3927-minimize-array-sum-using-divisible-replacements.java
+++ 3927-minimize-array-sum-using-divisible-replacements/3927-minimize-array-sum-using-divisible-replacements.java
@@ -1,44 +1,23 @@
 class Solution {
     public long minArraySum(int[] nums) {
-        // TC : O(nlogn + n*sqrt(n)*log(n))
-        // SC : constant
-        Arrays.sort(nums);
-        int n = nums.length;
-        long sum = 0;
-        for (int i = 0; i < n; i++) {
-            int num = nums[i];
-            int best = num;
-            for (int j = 1; j * j <= num; j++) {
-                if (num % j == 0) {
-                    if (check(nums, j)) {
-                        best = Math.min(best, j);
-                    }
-                    int val = num / j;
-                    if (check(nums, val)) {
-                        best = Math.min(best, val);
+        boolean[] vis = new boolean[100001];
+        for(int n : nums){
+            vis[n] = true;
+        }
+        int[] divisor = new int[100001];
+        for(int i=1; i<100001; i++){
+            if(vis[i]){
+                for(int j=i; j<100001; j+=i){
+                    if(divisor[j] == 0){
+                        divisor[j] = i;
                     }
                 }
             }
-            sum += best;
         }
+        long sum = 0;
+        for(int n : nums){
+            sum += divisor[n];
+        } 
         return sum;
-    }
-
-    public boolean check(int[] nums, int val) {
-        int lo = 0;
-        int hi = nums.length - 1;
-
-        while (lo <= hi) {
-            int mid = (lo + hi) / 2;
-
-            if (nums[mid] == val) {
-                return true;
-            } else if (nums[mid] > val) {
-                hi = mid - 1;
-            } else {
-                lo = mid + 1;
-            }
-        }
-        return false;
     }
 }
