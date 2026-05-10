320a74d5979c51890975970748cce73f5730af02
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2026-05-10 14:57:16 +0530

    Time: 1469 ms (6.56%), Space: 119.7 MB (33.64%) - LeetHub

--- 3927-minimize-array-sum-using-divisible-replacements/3927-minimize-array-sum-using-divisible-replacements.java
+++ 3927-minimize-array-sum-using-divisible-replacements/3927-minimize-array-sum-using-divisible-replacements.java
@@ -1,43 +1,44 @@
 class Solution {
     public long minArraySum(int[] nums) {
+        // TC : O(nlogn + n*sqrt(n)*log(n))
+        // SC : constant
+        Arrays.sort(nums);
         int n = nums.length;
         long sum = 0;
-        HashSet<Integer> set = new HashSet<>();
-        for(int i=0; i<n; i++){
-            set.add(nums[i]);
-        }
-        for(int i=0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             int num = nums[i];
-            int val = num;
-
-            for(int j=1; j*j<=num; j++){
-                if(num % j == 0){
-                    if(set.contains(j)){
-                        val = Math.min(val, j);
+            int best = num;
+            for (int j = 1; j * j <= num; j++) {
+                if (num % j == 0) {
+                    if (check(nums, j)) {
+                        best = Math.min(best, j);
                     }
-
-                    int half = num / j;
-                    if(set.contains(half)){
-                        val = Math.min(val, half);
+                    int val = num / j;
+                    if (check(nums, val)) {
+                        best = Math.min(best, val);
                     }
                 }
             }
-            sum += val;
+            sum += best;
         }
         return sum;
     }
+
+    public boolean check(int[] nums, int val) {
+        int lo = 0;
+        int hi = nums.length - 1;
+
+        while (lo <= hi) {
+            int mid = (lo + hi) / 2;
+
+            if (nums[mid] == val) {
+                return true;
+            } else if (nums[mid] > val) {
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return false;
+    }
 }
-
-// the range that could possibly divide the number completely it would be let's suppose
-// 1000 => [1, 500] would be the range 
-
-// if % is bigger
-
-// [2, 3, 4, 8] 2 + 3 + 2 + 2 
-// 2 -> [1] -> X
-// 3 -> [1] -> X
-// 4 -> [1,2] -> 2
-// 8 -> [1,4] -> 2
-
-// pair(a, b) -> a we have (b) we need to find
-// (1....b/2) -> In this range find the minimum that divides the number completely
