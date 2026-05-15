a8ef1473eea7a283e5ca06a6ec70a35847dae1e3
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2026-05-15 23:55:03 +0530

    Time: 1 ms (36.89%), Space: 46.8 MB (60.6%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,36 +1,38 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        // BFS
-        // TC : O(m*n)
-        // SC : O(m*n)
-        int m = image.length;
-        int n = image[0].length;
+        // TC : O(n*m*4)
+        // SC : O(n*m)
+        int srcClr = image[sr][sc]; // src clr
+        int[] rDir = {-1, 0, 1, 0};
+        int[] cDir = {0, 1, 0, -1};
         Queue<int[]> q = new LinkedList<>();
         q.offer(new int[]{sr, sc});
-
-        int[] delRow = {-1, 0, 1, 0};
-        int[] delCol = {0, 1, 0, -1};
-        int sourceCellColor = image[sr][sc];
+        boolean[][] vis = new boolean[image.length][image[0].length];
+        vis[sr][sc] = true;
         image[sr][sc] = color;
 
         while(!q.isEmpty()){
-            int currRow = q.peek()[0];
-            int currCol = q.peek()[1];
-            q.poll();
+            int size = q.size();
+            for(int i=0; i<size; i++){
+                int[] curr = q.poll();
+                int r = curr[0];
+                int c = curr[1];
 
-            for(int i=0; i<4; i++){
-                int nebRow = currRow + delRow[i];
-                int nebCol = currCol + delCol[i];
+                for(int k=0; k<4; k++){
+                    int nbrR = r + rDir[k];
+                    int nbrC = c + cDir[k];
 
-                if(nebRow >= 0 && nebRow < m
-                && nebCol >= 0 && nebCol < n
-                && image[nebRow][nebCol] == sourceCellColor
-                && image[nebRow][nebCol] != color){
-                    image[nebRow][nebCol] = color;
-                    q.offer(new int[]{nebRow, nebCol});
+                    if(nbrR >= 0 && nbrR < image.length 
+                    && nbrC >=0 && nbrC < image[0].length
+                    && image[nbrR][nbrC] == srcClr && !vis[nbrR][nbrC]){
+                        vis[nbrR][nbrC] = true;
+                        q.offer(new int[]{nbrR, nbrC});
+                        image[nbrR][nbrC] = color;
+                    }
                 }
             }
         }
+
         return image;
     }
 }
