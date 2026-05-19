c9bd66a8ea99f55fce8b6aaed4daf0f8ed4c229b
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2026-05-20 02:02:02 +0530

    Time: 5 ms (97.74%), Space: 44.1 MB (62.25%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,28 +1,34 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int m = s1.length();
-        int n = s2.length();
-        int[] arr1 = new int[26];
-        int[] arr2 = new int[26];
-        for(int i=0; i<m; i++){
-            arr1[s1.charAt(i) - 'a']++;
+        // TC : O(n + m*26)
+        // SC : O(26 * 2)
+        int n = s1.length();
+        int m = s2.length();
+
+        int[] s1Map = new int[26];
+        int[] s2Map = new int[26];
+        for(int i=0; i<n; i++){
+            s1Map[s1.charAt(i) - 'a']++;
         }
 
-        int i=0;
-        int j=0;
-        while(j < n){
-            arr2[s2.charAt(j) - 'a']++;
+        int i = 0;
+        int j = 0;
 
-            if(j - i + 1 == m){
-                if(check(arr1, arr2)) return true;
-                arr2[s2.charAt(i) - 'a']--;
+        while(j < m){
+            s2Map[s2.charAt(j) - 'a']++;
+
+            if(j - i + 1 == n){
+                if(check(s1Map, s2Map)){
+                    return true;
+                }
+
+                s2Map[s2.charAt(i) - 'a']--;
                 i++;
             }
             j++;
         }
         return false;
     }
-
     public boolean check(int[] arr1, int[] arr2){
         for(int i=0; i<26; i++){
             if(arr1[i] != arr2[i]){
