725fa39beb6d644130a0906bff4fb46b721736a8
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2026-05-21 01:15:48 +0530

    Time: 28 ms (90.11%), Space: 147.8 MB (41.26%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,29 +1,25 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        int[] ans = new int[n - k + 1];
+        Deque<int[]> dq = new ArrayDeque<>();
 
-       int n = nums.length;
-       if(n == 0){
-           return nums;
-       } 
-       int[] ans = new int[n - k + 1];
-       Deque<Integer> dq = new LinkedList<>();
-       int idx = 0;
+        for(int i=0; i<n; i++){
 
-       for(int i=0; i<n; i++){
-           
-           int currEle = nums[i];
-           while(!dq.isEmpty() && nums[dq.peekLast()] < currEle){
-               dq.removeLast();
-           }
-           if(!dq.isEmpty() && dq.peek() < i-k+1){
-               dq.remove();
-           }
-           dq.add(i);
+            while(!dq.isEmpty() && dq.getLast()[1] <= nums[i]){
+                dq.removeLast();
+            }
 
-           if(i >= k-1){
-               ans[idx++] = nums[dq.peek()];
-           }
-       }
-       return ans;
+            if(!dq.isEmpty() && dq.peek()[0] < i-k+1){
+                dq.remove();
+            }
+
+            dq.addLast(new int[]{i, nums[i]});
+
+            if(i - k + 1 >= 0){
+                ans[i - k + 1] = dq.getFirst()[1];
+            }
+        }
+        return ans;
     }
 }
