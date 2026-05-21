beb8e34d14c32db32f569e609b36d71d147ccfec
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2026-05-22 02:24:22 +0530

    Time: 0 ms (100%), Space: 42.8 MB (58.96%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,30 +1,31 @@
 class Solution {
     public int rob(int[] nums) {
-        // TC : O(n)
-        // SC : O(n)
         int n = nums.length;
-        if(n == 2) return Math.max(nums[0], nums[1]);
-        if(n == 1) return nums[0];
-        int[] dp = new int[n+1];
+        if(n <= 2){
+            if(n == 1) return nums[0];
+            return Math.max(nums[0], nums[1]);
+        }
+        int[] dp = new int[n];
         Arrays.fill(dp, -1);
-        int ans1 = solve(nums, 0, n-1, dp);
+        int max1 = solve(nums, 0, n-1, dp);
         Arrays.fill(dp, -1);
-        int ans2 = solve(nums, 1, n, dp);
-        return Math.max(ans1, ans2);
+        int max2 = solve(nums, 1, n, dp);
+
+        return Math.max(max1, max2);
     }
 
-    public int solve(int[] nums, int i, int n, int[] dp){
+    public int solve(int[] nums, int i, int n, int[] dp) {
         // base case
-        if(i >= n) {
+        if (i >= n) {
             return 0;
         }
-        if(dp[i] != -1){
+        if (dp[i] != -1) {
             return dp[i];
         }
 
-        int take = nums[i] + solve(nums, i+2, n, dp);
-        int not_take = 0 + solve(nums, i+1, n, dp);
+        int idx_plus_two = nums[i] + solve(nums, i + 2, n, dp);
+        int idx_plus_one = 0 + solve(nums, i + 1, n, dp);
 
-        return dp[i] = Math.max(take, not_take);
+        return dp[i] = Math.max(idx_plus_two, idx_plus_one);
     }
 }
