952e43a6481f8abfdc060cc0e9ed5de361212e68
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2026-05-22 23:47:37 +0530

    Time: 14 ms (70.89%), Space: 47.4 MB (80.1%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,35 +1,34 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        // TC : O(nlogk)
-        // SC : O(n)
         int n = nums.length;
         HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i=0; i<n; i++){
-            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[1], b[1]));
+
+        for (int i = 0; i < n; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
         }
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
-            if(a[0] == b[0]){
-                return a[1] - b[1];
-            }
-            return a[0] - b[0];
-        });
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int key = entry.getKey();
+            int val = entry.getValue();
 
-        for(int key : map.keySet()){
-            int V = map.get(key);
-            int K = key;
-
-            pq.offer(new int[]{V, K});
-            if(pq.size() > k){
+            pq.offer(new int[] { key, val });
+            if (pq.size() > k) {
                 pq.poll();
             }
         }
 
         int[] ans = new int[Math.min(k, pq.size())];
-        int i = 0;
-        while(!pq.isEmpty()){
-            ans[i++] = pq.poll()[1];
+        int i = ans.length - 1;
+        while (!pq.isEmpty()) {
+            ans[i--] = pq.poll()[0];
         }
         return ans;
     }
 }
+
+// [1,2,1,2,1,2,3,1,3,2], k = 2
+// 1 -> 4, 2 -> 4, 3 -> 2
+// [1, 4], [2, 4], [3, 2] map
+// pq -> {1, 4}{2,4}{3,2} top k = [1, 2]
+// treemap also can be used
