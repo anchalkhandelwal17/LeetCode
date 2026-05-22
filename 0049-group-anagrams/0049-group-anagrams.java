1756ef33801f6faef636252fca734fa5ba933449
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2026-05-22 23:35:37 +0530

    Time: 6 ms (99.45%), Space: 49.6 MB (72.58%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,10 +1,8 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        // TC : O(n * max(strs[i]log(strs[i])))
+        // TC : O(n*mlogm)
         int n = strs.length;
-        List<List<String>> ans = new ArrayList<>();
-        Map<String, List<String>> map = new HashMap<>();
-
+        HashMap<String, List<String>> map = new HashMap<>();
         for(int i=0; i<n; i++){
             String str = strs[i];
             char[] ch = str.toCharArray();
@@ -15,9 +13,6 @@ class Solution {
             }
             map.get(newStr).add(str);
         }
-        for(String k : map.keySet()){
-            ans.add(map.get(k));
-        }
-        return ans;
+        return new ArrayList<>(map.values());
     }
 }
