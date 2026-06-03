969c0de028b574d97e99c5cb388f67b93ee5ef31
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2026-06-03 23:45:36 +0530

    Time: 7 ms (14.35%), Space: 48.7 MB (29.57%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,12 +1,16 @@
 class Solution {
     public int[] countBits(int n) {
-        int[] ans = new int[n+1];
+        // TC : O(n * 32)
+        // SC : O(n)
+        int[] ans = new int[n + 1];
         for(int i=0; i<=n; i++){
-            int num = i;
+            int temp = i;
             int cnt = 0;
-            while(num != 0){
-                if((num & 1) == 1) cnt++;
-                num >>= 1;
+            for(int j=0; j<32; j++){
+                // temp >>= j;
+                if(((temp >> j) & 1) == 1){
+                    cnt++;
+                }
             }
             ans[i] = cnt;
         }
