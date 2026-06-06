6716ca0f10d8d92ff695edca86ebe8351af043be
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2026-06-07 00:02:18 +0530

    Time: 349 ms (15.01%), Space: 236.5 MB (19.95%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,29 +1,37 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        int[][] dp = new int[n][n+1];
-
+        int[][] dp = new int[n][n];
         for(int[] arr : dp){
             Arrays.fill(arr, -1);
         }
-
+        
         return lis(nums, 0, -1, dp);
     }
 
-    public int lis(int[] nums, int idx, int prev_idx, int[][] dp){
-
-        // base case
-        if(idx >= nums.length) return 0;
-
-        if(dp[idx][prev_idx+1] != -1) return dp[idx][prev_idx+1];
-
-        int take = 0;
-        if(prev_idx == -1 || nums[idx] > nums[prev_idx]){
-            take = 1 + lis(nums, idx+1, idx, dp);
+    public int lis(int[] nums, int i, int prev_idx, int[][] dp){
+        // base;
+        if(i >= nums.length){
+            return 0;
+        }
+        if(prev_idx != -1 && dp[i][prev_idx] != -1){
+            return dp[i][prev_idx];
         }
 
-        int not_take = 0 + lis(nums, idx+1, prev_idx, dp);
+        int pick = 0;
+        if(prev_idx == -1 || nums[i] > nums[prev_idx]){
+            pick = 1 + lis(nums, i+1, i, dp);
+        }
 
-        return dp[idx][prev_idx+1] = Math.max(take, not_take);
+        int not_pick = 0 + lis(nums, i+1, prev_idx, dp);
+
+        if(prev_idx != -1){
+            dp[i][prev_idx] = Math.max(pick, not_pick);
+        }
+
+        return Math.max(pick, not_pick);
     }
 }
+
+// i > prev i
+// if(i > prev i) still take or not_take cases
