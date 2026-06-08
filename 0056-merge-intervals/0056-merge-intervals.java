172bfbba063d6071e7f96b81b2b6fb05a74c5334
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2026-06-09 01:01:06 +0530

    Time: 10 ms (21.27%), Space: 49.1 MB (56.89%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,17 +1,9 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        // TC : O(nlogn)
-        // SC : O(n)
-        int n = intervals.length;
-        List<int[]> list = new ArrayList<>();
-        Arrays.sort(intervals, (a, b) -> {
-            if(a[0] == b[0]){
-                return a[1] - b[1];
-            }
-            return a[0] - b[0];
-        });
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
 
-        for(int i=0; i<n; i++){
+        List<int[]> list = new ArrayList<>();
+        for(int i=0; i<intervals.length; i++){
             int a = intervals[i][0];
             int b = intervals[i][1];
 
@@ -19,17 +11,18 @@ class Solution {
                 list.add(new int[]{a, b});
             }
             else{
-                list.get(list.size() - 1)[1] = Math.max(list.get(list.size() - 1)[1], b);
+                list.get(list.size()-1)[1] = Math.max(list.get(list.size()-1)[1], b);
             }
         }
 
         int[][] ans = new int[list.size()][2];
+        for(int i=0; i<list.size(); i++){
+            int a = list.get(i)[0];
+            int b = list.get(i)[1];
 
-        for(int i=0; i<ans.length; i++){
-            ans[i][0] = list.get(i)[0];
-            ans[i][1] = list.get(i)[1];
+            ans[i][0] = a;
+            ans[i][1] = b;
         }
-
         return ans;
     }
 }
