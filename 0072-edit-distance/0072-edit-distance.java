0bf998d1a410acb17c7884d043d7badbf90c4c21
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2026-07-17 03:21:22 +0530

    Time: 5 ms (68.52%), Space: 47.3 MB (34.86%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,7 +1,7 @@
 class Solution {
     public int minDistance(String word1, String word2) {
         // TC : O(m*n)
-        // SC : O(m*n + m+n)
+        // SC : O(m*n)
         int m = word1.length();
         int n = word2.length();
         int[][] dp = new int[m][n];
@@ -12,12 +12,18 @@ class Solution {
     }
 
     public int solve(String a, String b, int i, int j, int[][] dp){
-        // base
-        if(i == a.length() && j == b.length()) return 0;
-        if(i >= a.length()) return b.length() - j;
-        if(j >= b.length()) return a.length() - i;
-        if(dp[i][j] != -1) return dp[i][j];
-        
+        // base case
+        if(i >= a.length()){
+            return b.length() - j;
+        }
+        if(j >= b.length()){
+            return a.length() - i;
+        }
+
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+
         if(a.charAt(i) == b.charAt(j)){
             return solve(a, b, i+1, j+1, dp);
         }
@@ -29,7 +35,11 @@ class Solution {
     }
 }
 
-// we need to convert word1 to word2
-// insert a char will be 1 + (i, j+1)
-// delete a char will be 1 + (i+1, j)
-// replace a char will be 1 + (i+1, j+1)
+// need to convert word1 to word2
+// three ops are permitted 
+// insert, delete, replace
+
+// if char matches in both the strings word1 and word2
+// simply move bothe the pointers i and j
+// otherwise try to insert, delete and replace as well
+// and take min ans
